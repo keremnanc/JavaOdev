9c1a0ae0bded9426a3e1fe2bb96ebe0d11d25a52
Kerem İnanç <64323516+keremnanc@users.noreply.github.com>
2021-04-27 03:11:48 +0300

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,26 @@
+package oopIntro;
+
+public class Comment {
+
+	public Comment() 
+	{
+		
+	}
+	public Comment(String name, int puanlama,String detail)
+	{
+		this();
+		this.name=name;
+		this.puanlama=puanlama;
+		this.detail=detail;
+		
+		
+	}
+	
+	String name;
+	int puanlama;
+	String detail;
+	
+	
+	
+	
+}
---
+++ Course.java
@@ -0,0 +1,25 @@
+package oopIntro;
+
+public class Course {
+	
+	public Course() 
+	{
+		
+	}
+	public Course (String name,double unitPrice,String detail)
+	{
+		
+	
+	this();
+	this.name=name;
+	this.unitPrice=unitPrice;
+	this.detail=detail;
+	
+	}
+	
+	
+	String name;
+	double unitPrice;
+	String detail;
+	
+}
